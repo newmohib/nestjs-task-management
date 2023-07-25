f2928cb4683e8129496b9a8d142e6f12a5edded8
mohibbs <mohib@bs-23.net>
2023-07-26 02:20:21 +0600

    update: task for filter

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,22 +1,32 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
-import { title } from 'process';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { log } from 'console';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) { }
 
     @Get()
-    getAllTasks(): Task[] {
-        title
-        return this.tasksService.getAllTasks()
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        // if we have any filters defined, call tasksService.getTaskWithFilters
+        // otherwise, just get all tasks
+        
+        if (Object.keys(filterDto).length) {
+            // call filter functions
+           return this.tasksService.getTasksWithFilters(filterDto)
+
+        }else{
+            return this.tasksService.getAllTasks()
+        }
+
     }
 
     @Get('/:id')
     getTaskById(@Param('id') id: string): Task {
-        title
+        
         return this.tasksService.getTaskById(id)
     }
 
@@ -38,7 +48,7 @@ export class TasksController {
 
     @Delete('/:id')
     deleteTaskById(@Param('id') id: string): Boolean {
-        title
+        
         this.tasksService.deleteTaskById(id);
         return true;
     }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import {Task,TaskStatus} from './task.model';
 import {v4 as uuid} from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,38 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+    
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[]{
+        const {search, status} = filterDto;
+
+        // define a temporary array to hold the result
+        let tasks = this.getAllTasks();
+
+        // do something with status
+
+        if (status) {
+            tasks = tasks.filter(task=> task.status === status);
+        }
+        
+        // do somethign with search
+
+        if (search) {
+            tasks = tasks.filter(task=> {
+                if (task.title.includes(search) || task.description.includes(search)) {
+                    return true;
+                }else{
+                    return false;
+                }
+            });
+            
+        }
+
+        //console.log(tasks, search, status)
+        // return final result
+
+        return tasks;
+        
     }
 
     getTaskById(id: string): Task {
